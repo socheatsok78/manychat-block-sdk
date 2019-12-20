3f5799bbfe622022620964f6e7c4a852a910268a
Socheat Sok <alex@socheat.net>
2019-12-20 17:54:02 +0700

    Create Messages/CardHorizontal.php && Messages/CardSquare.php

---
+++ src/Messages/CardHorizontal.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ManyChat\Dynamic\Messages;
+
+use ManyChat\Dynamic\Foundation\CardBlock;
+
+class CardHorizontal extends CardBlock
+{
+    /**
+     * The List style
+     *
+     * @var string
+     */
+    protected $style = 'horizontal';
+
+    /**
+     * Create a new List instance
+     *
+     * @param array $items
+     * @return self
+     */
+    public static function create(array $items)
+    {
+        return new self($items);
+    }
+}
