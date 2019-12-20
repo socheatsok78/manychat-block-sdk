dc8aa3ff374794747f24ef3b371a540eafa13223
Socheat Sok <alex@socheat.net>
2019-12-20 11:50:14 +0700

    Create Attachments/Video.php

---
+++ src/Attachments/Video.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace ManyChat\Dynamic\Attachments;
+
+use ManyChat\Dynamic\Attachments\Attachment;
+
+class Video extends Attachment
+{
+    /**
+     * The Attachment type
+     *
+     * @var string
+     */
+    protected $type = "video";
+
+    /**
+     * The Video url
+     *
+     * @var string
+     */
+    protected $url;
+
+    /**
+     * Attachment constructor
+     * @param string $url
+     * @param mixed $payload
+     */
+    public function __construct($url, $payload = null)
+    {
+        parent::__construct($payload);
+
+        $this->url = $url;
+    }
+
+    /**
+     * Create a new Video instance
+     *
+     * @param string $url
+     * @return File
+     */
+    public static function url(string $url)
+    {
+        return new self($url);
+    }
+
+    /**
+     * Get the Video url
+     *
+     * @return void
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Get the Attachement elements
+     *
+     * @return array
+     */
+    protected function elements()
+    {
+        return [
+            'url' => $this->getUrl()
+        ];
+    }
+}
