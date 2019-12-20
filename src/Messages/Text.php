37af95ba327de40aa8a470eb5e622730de76731a
Socheat Sok <alex@socheat.net>
2019-12-20 17:53:20 +0700

    Create Messages/Text.php

---
+++ src/Messages/Text.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace ManyChat\Dynamic\Messages;
+
+use ManyChat\Dynamic\Messages\Message;
+
+class Text extends Message
+{
+    /**
+     * The Message type
+     *
+     * @var string
+     */
+    protected $type = "text";
+
+    /**
+     * The Message text
+     *
+     * @var string
+     */
+    protected $text;
+
+    /**
+     * Attachment constructor
+     * @param string $text
+     * @param mixed $payload
+     */
+    public function __construct($text, $payload = null)
+    {
+        parent::__construct($payload);
+
+        $this->text = $text;
+    }
+
+
+    /**
+     * Create a new Text instance
+     *
+     * @param string $text
+     * @return self
+     */
+    public static function text(string $text)
+    {
+        return new self($text);
+    }
+
+    /**
+     * Get the Message text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Get the Message elements
+     *
+     * @return array
+     */
+    protected function elements()
+    {
+        return [
+            'text' => $this->getText()
+        ];
+    }
+}
