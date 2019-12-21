fcd6649b7c5b4aa03b92d463c5c4f9244cc5208c
Socheat Sok <alex@socheat.net>
2019-12-21 20:24:13 +0700

    Update Messages/Element.php, add method to set data

--- src/Messages/Element.php
+++ src/Messages/Element.php
@@ -20,4 +20,56 @@ class Element extends BaseElement
             $this->actionUrl = $data['action_url'];
         }
     }
+
+    /**
+     * Set the Element title
+     *
+     * @param string $title
+     * @return self
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Set the Element sub-title
+     *
+     * @param string $subTitle
+     * @return self
+     */
+    public function setSubTitle($subTitle)
+    {
+        $this->subTitle = $subTitle;
+
+        return $this;
+    }
+
+    /**
+     * Set the Element image
+     *
+     * @param string $url
+     * @return self
+     */
+    public function setImage($url)
+    {
+        $this->imageUrl = $url;
+
+        return $this;
+    }
+
+    /**
+     * Set the Element action
+     *
+     * @param string $action
+     * @return self
+     */
+    public function setAction($action)
+    {
+        $this->actionUrl = $action;
+
+        return $this;
+    }
 }
