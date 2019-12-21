3ded870242e133d277e68de4d520bf62db8bbeba
Socheat Sok <alex@socheat.net>
2019-12-21 22:54:39 +0700

    Remove unnecessary methods from Messages/Element.php

--- src/Messages/Element.php
+++ src/Messages/Element.php
@@ -15,61 +15,9 @@ class Element extends BaseElement
     {
         if ($data) {
             $this->title = $data['title'];
-            $this->subTitle = $data['subtitle'];
-            $this->imageUrl = $data['image_url'];
-            $this->actionUrl = $data['action_url'];
+            $this->subTitle = $data['subTitle'];
+            $this->imageUrl = $data['imageUrl'];
+            $this->actionUrl = $data['actionUrl'];
         }
-    }
-
-    /**
-     * Set the Element title
-     *
-     * @param string $title
-     * @return self
-     */
-    public function setTitle($title)
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    /**
-     * Set the Element sub-title
-     *
-     * @param string $subTitle
-     * @return self
-     */
-    public function setSubTitle($subTitle)
-    {
-        $this->subTitle = $subTitle;
-
-        return $this;
-    }
-
-    /**
-     * Set the Element image
-     *
-     * @param string $url
-     * @return self
-     */
-    public function setImage($url)
-    {
-        $this->imageUrl = $url;
-
-        return $this;
-    }
-
-    /**
-     * Set the Element action
-     *
-     * @param string $action
-     * @return self
-     */
-    public function setAction($action)
-    {
-        $this->actionUrl = $action;
-
-        return $this;
     }
 }
