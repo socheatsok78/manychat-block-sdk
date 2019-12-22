26939ee2a08f46b812dd32016021f1a902bbaf13
Socheat Sok <alex@socheat.net>
2019-12-22 14:59:29 +0700

    Update Callback/Callback.php, add methods to set header and payload

--- src/Callback/Callback.php
+++ src/Callback/Callback.php
@@ -7,6 +7,68 @@ use ManyChat\Dynamic\Foundation\Block;
 abstract class Callback extends Block
 {
     /**
+     * Set Dynamic Block HTTP request header
+     *
+     * @param string $key
+     * @param string $value
+     * @return self
+     */
+    public function setHeader($key, $value)
+    {
+        $this->headers[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set Dynamic Block HTTP request headers
+     *
+     * @param array $headers
+     * @return void
+     */
+    public function setHeaders($headers)
+    {
+        if (!is_array($this->headers)) {
+            $this->headers = [];
+        }
+
+        array_merge($this->headers, $headers);
+
+        return $this;
+    }
+
+    /**
+     * Set Dynamic Block HTTP request payload
+     *
+     * @param string $key
+     * @param string $value
+     * @return self
+     */
+    public function setPayload($key, $value)
+    {
+        $this->payload[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set Dynamic Block HTTP request payloads
+     *
+     * @param array $payload
+     * @return void
+     */
+    public function setPayloads($payload)
+    {
+        if (!is_array($this->payload)) {
+            $this->payload = [];
+        }
+
+        array_merge($this->payload, $payload);
+
+        return $this;
+    }
+
+    /**
      * Check if a Block can have buttons element
      *
      * @return boolean
