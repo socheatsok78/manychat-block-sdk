ddfd66acb3f2f6ba1c730fd01dc1657adb46cad1
Socheat Sok <alex@socheat.net>
2019-12-24 09:13:46 +0700

    Create src/Subscriber.php

---
+++ src/Subscriber.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ManyChat\Dynamic;
+
+use ManyChat\Dynamic\Foundation\Subscriber as BaseSubscriber;
+
+class Subscriber extends BaseSubscriber
+{
+
+
+    /**
+     * Create a new Subscriber instance
+     *
+     * @param mixed $subscriber
+     */
+    public function __construct($subscriber)
+    {
+        parent::__construct($subscriber);
+    }
+}
