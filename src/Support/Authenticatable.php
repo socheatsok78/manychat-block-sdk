dd92188a583d2c4e5d2a17974c6d6a617c8f35fe
Socheat Sok <alex@socheat.net>
2019-12-26 10:55:40 +0700

    Create Support/Authenticatable.php

---
+++ src/Support/Authenticatable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ManyChat\Dynamic\Support;
+
+interface Authenticatable
+{
+    /**
+     * Get the name of the unique identifier for the user.
+     *
+     * @return string
+     */
+    public function getAuthIdentifierName();
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier();
+}
