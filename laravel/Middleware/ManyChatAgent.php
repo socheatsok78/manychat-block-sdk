fac3a578fe55cff72fe975acf82eebabd384d230
Socheat Sok <alex@socheat.net>
2019-12-26 16:27:59 +0700

    Refactor Laravel/Middleware/ManyChatAgent to use ManyChatMiddleware base class

--- laravel/Middleware/ManyChatAgent.php
+++ laravel/Middleware/ManyChatAgent.php
@@ -2,49 +2,11 @@
 
 namespace ManyChat\Laravel\Middleware;
 
-use \Closure;
-use ManyChat\Dynamic\Chat;
-use Illuminate\Http\Request;
-use Illuminate\Contracts\Routing\ResponseFactory;
-use Illuminate\Contracts\Routing\UrlGenerator;
+use Closure;
+use ManyChat\Laravel\Middleware\ManyChatMiddleware;
 
-class ManyChatAgent
+class ManyChatAgent extends ManyChatMiddleware
 {
-    /**
-     * The response factory instance.
-     *
-     * @var \Illuminate\Contracts\Routing\ResponseFactory
-     */
-    protected $responseFactory;
-
-    /**
-     * The URL generator instance.
-     *
-     * @var \Illuminate\Contracts\Routing\UrlGenerator
-     */
-    protected $urlGenerator;
-
-    /**
-     * The request instance.
-     *
-     * @var \Illuminate\Http\Request
-     */
-    protected $request;
-
-    /**
-     * Create a new middleware instance.
-     *
-     * @param  \Illuminate\Contracts\Routing\ResponseFactory  $responseFactory
-     * @param  \Illuminate\Contracts\Routing\UrlGenerator  $urlGenerator
-     * @return void
-     */
-    public function __construct(Request $request, ResponseFactory $responseFactory, UrlGenerator $urlGenerator)
-    {
-        $this->request = $request;
-        $this->responseFactory = $responseFactory;
-        $this->urlGenerator = $urlGenerator;
-    }
-
     /**
      * Handle an incoming request.
      *
@@ -54,8 +16,6 @@ class ManyChatAgent
      */
     public function handle($request, Closure $next)
     {
-        $userAgent = $request->header('User-Agent');
-
         if (!$this->isManyChat()) {
             $statusCode = 406;
             $errorMessage = "Invalid ManyChat Agent";
@@ -70,21 +30,5 @@ class ManyChatAgent
         }
 
         return $next($request);
-    }
-
-    /**
-     * Check if request is from ManyChat
-     *
-     * @return boolean
-     */
-    protected function isManyChat()
-    {
-        $request = $this->request;
-
-        $requestAgent = $request->header('User-Agent');
-
-        $manyChatAgent = Chat::HEADER_USER_AGENT;
-
-        return preg_match("/$manyChatAgent/i", $requestAgent);
     }
 }
